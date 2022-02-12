ff43e5fcf4a3995b4905b01c57a55d022c613349
kolatham <92490383+kolatham@users.noreply.github.com>
2022-02-11 20:12:00 -0500

    finished index.js in controllers

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
 
+const apiRoutes = require('./api');
+const homeRoute = require('./homeRoutes')
+
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
