c090f4c5fc9fcc335a308aaa77d2582d8c89d645
kolatham <92490383+kolatham@users.noreply.github.com>
2022-02-15 15:11:34 -0500

    added to a link

--- Develop/public/css/js/login.js
+++ Develop/public/css/js/login.js
@@ -7,7 +7,7 @@ const loginFormHandler = async (event) => {
 
     if (email && password) {
         console.log(email)
-        const response = await fetch ('/api/users/login', {
+        const response = await fetch ('/controllers/api/users/login', {
             method: 'POST',
             body: JSON.stringify({ email: email, password: password,}),
             headers: {'Content-Type': 'application/json'},
